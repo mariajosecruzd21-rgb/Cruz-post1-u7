92d48bbd11255b92c695ab444f52f749803d313c
mariajosecruz <02230131003@mail.udes.edu.co>
2026-04-20 17:28:24 -0500

    Creacion del diagrama con ejecucion correcta

---
+++ src/main/java/com/universidad/patrones/tareas/CruzPost1U7Application.java
@@ -0,0 +1,13 @@
+package com.universidad.patrones.tareas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruzPost1U7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CruzPost1U7Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/universidad/patrones/gelvezpost1u7/CruzPost1U7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.universidad.patrones.gelvezpost1u7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruzPost1U7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
